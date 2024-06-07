39a15ebe16c35ddcbe9386d0093c4c5fd7dde5af
seanwolfe <sean.wolfe@hotmail.com>
2024-06-07 17:12:05 -0400

    test set 2 for completion

--- TestSetGen.py
+++ TestSetGen.py
@@ -11,8 +11,40 @@ import yaml
 class TestSetGen:
 
     def __init__(self, config):
-        # Theta, Sigma_g, Expected SNR, Original Image, Crop start, Center x, Center y
         self.configuration = config
+        return
+
+
+    def set_2(self):
+
+        num_samples_per_bin = self.configuration['num_samples_per_bin']
+        num_bins = self.configuration['num_bins']
+
+        min_omega = self.configuration['min_omega']
+        max_omega = self.configuration['max_omega']
+        omegas = np.linspace(min_omega, max_omega, num=num_bins * num_samples_per_bin)
+
+        min_v = self.configuration['min_apparent_magnitude']
+        max_v = self.configuration['max_apparent_magnitude']
+        vs = np.linspace(min_v, max_v, num=num_bins * num_samples_per_bin)
+
+        min_sigma = self.configuration['min_sigma_g']
+        max_sigma = self.configuration['max_sigma_g']
+        sigmas = np.linspace(min_sigma, max_sigma, num_bins * num_samples_per_bin)
+
+        thetas = np.random.uniform(0, 360, num_bins * num_samples_per_bin)
+
+        df = pd.DataFrame([omegas, vs, thetas, sigmas], columns=['omega', 'Apparent Magnitude', 'Theta', 'Sigma_g'])
+
+        #save
+
+        return
+
+
+
+
+    def set_1(self):
+        # Theta, Sigma_g, Expected SNR, Original Image, Crop start, Center x, Center y
         self.configuration['num_stacks'] = 1
         self.background = BackgroundGen(self.configuration)
         self.signal_gen = SignalGen(self.configuration)
@@ -41,9 +73,7 @@ class TestSetGen:
         self.testset = pd.concat([testset_1, signal_calc])
         self.testset.reset_index(drop=True, inplace=True)
         self.testset.to_csv(os.path.join(self.configuration['test_set_directory'], 'testset.csv'), sep=',', index=False,
-                       header=True)
-
-        return
+                            header=True)
 
 
 if __name__ == '__main__':
